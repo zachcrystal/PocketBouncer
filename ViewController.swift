00bae6021eb7248a20782015255dcef232788977
zachcrystal <Zach@zachs-macbook-pro.local>
2017-06-11 23:38:55 -0400

    fixed button, added countdown (no functionality)

--- ViewController.swift
+++ ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController {
         }
     }
     
+    var countdown = 30
+    
     var exponentialMessUp: Int = 1
     
     var scoreLabel: UILabel = {
@@ -30,6 +32,14 @@ class ViewController: UIViewController {
         label.font = UIFont.boldSystemFont(ofSize: 16)
         return label
     }()
+    
+    var timerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "30"
+        label.font = UIFont.boldSystemFont(ofSize: 28)
+        return label
+    }()
+
     
     var largeSquare: UIImageView = {
         let iv = UIImageView()
@@ -114,14 +124,7 @@ class ViewController: UIViewController {
         let wrongLetInTitle = "Hey! You let someone in you weren't supposed to!"
         let message = "Incorrect: Score - \(10 * exponentialMessUp)"
         let answerAlert = UIAlertController(title: getLost ? getLostTitle : wrongLetInTitle, message: message, preferredStyle: .alert)
-        let resetAction = UIAlertAction(title: "Reset", style: .destructive) { (_) in
-            self.score = 0
-            self.exponentialMessUp = 1
-            self.setImages()
-        }
-        answerAlert.addAction(resetAction)
-
-        let nextRoundAction = UIAlertAction(title: "Next Round", style: .default) { (_) in
+        let nextRoundAction = UIAlertAction(title: "Sorry! It won't happen again!", style: .default) { (_) in
             self.setImages()
         }
         answerAlert.addAction(nextRoundAction)
@@ -146,11 +149,16 @@ class ViewController: UIViewController {
         view.addSubview(stackView)
         view.addSubview(containerView)
         view.addSubview(scoreLabel)
+        view.addSubview(timerLabel)
+        
+        var _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
+        
 
         setImages()
         
         // anchoring views using the anchoring extension in Extensions.swift
-        largeSquare.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 100, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 200)
+        largeSquare.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 140, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 200)
         largeSquare.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         stackView.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, width: 0, height: 70)
@@ -161,8 +169,21 @@ class ViewController: UIViewController {
         
         scoreLabel.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 30, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
+        timerLabel.anchor(top: nil, left: nil, bottom: largeSquare.topAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 30, paddingRight: 0, width: 0, height: 30)
+        timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
         
     }
+    
+    func updateCounter() {
+        //you code, this is an example
+        if countdown > 0 {
+            countdown -= 1
+            timerLabel.text = "\(countdown)"
+        }
+    }
+    
+    
     
     // first make a copy of colorsArray so we can remove the random color selected for the large square. The color is removed because we handle the option that the colors match using a random number between 1 and 100. If the number less than 60, the smaller square is set to the same color as the large square (a match) and if the number is greater than 60, a random color is chosen from the 15 remaining colors in the array.
     
