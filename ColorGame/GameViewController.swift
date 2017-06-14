efe111258e11e446b6ca86eb5b24643c78be4c3d
zachcrystal <Zach@zachs-macbook-pro.local>
2017-06-13 22:43:33 -0400

    refactoring for free play

--- ColorGame/GameViewController.swift
+++ ColorGame/GameViewController.swift
@@ -11,100 +11,40 @@ import AVFoundation
 
 class GameViewController: UIViewController {
     
-    var audioPlayer: AVAudioPlayer?
-    
-    func playMusic() {
-        guard let sound = NSDataAsset(name: "MusicLoop3") else {
-            print("asset not found")
-            return
-        }
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            audioPlayer = try AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
-            
-            audioPlayer!.play()
-            audioPlayer!.numberOfLoops = -1
-            
-        } catch let error as NSError {
-            print("error: \(error.localizedDescription)")
-        }
-    }
-    
-    func stopAudio() {
-        if audioPlayer != nil {
-            audioPlayer?.stop()
-            audioPlayer = nil
-        }
-    }
-    
     var people: [Person]?
     
     var idIsExpired: Bool?
     var legalAge: Bool?
     
-    var lives = 3 {
+    var highScore: Int = 0
+    
+    var score: Int = 0 {
         didSet {
-            if lives == 0 {
-                // do something
-            }
+            scoreLabel.text = "\(score)"
         }
     }
     
-    var peopleLetIn: Int = 0 {
-        didSet {
-            letInLabel.text = "People let in: \(peopleLetIn)"
-        }
-    }
     
-    var countdown = 30 {
-        didSet {
-            if countdown == 0 {
-                endOfRound()
-            }
-        }
-    }
-    
-    func endOfRound() {
+    fileprivate func gameover() {
         let endOfRoundTitle = "Your shift for the night is finally over! Lets see how you did..."
-        let endOfRoundMessage = "You let in \(peopleLetIn) people"
+        let endOfRoundMessage = "You let in \(score) people"
         let answerAlert = UIAlertController(title: endOfRoundTitle, message: endOfRoundMessage, preferredStyle: .alert)
-        let nextRoundAction = UIAlertAction(title: "Start next shift", style: .default) { (_) in
-            self.startNewRound()
-        }
+        let nextRoundAction = UIAlertAction(title: "Start next shift", style: .default)
         answerAlert.addAction(nextRoundAction)
         
         present(answerAlert, animated: true, completion: nil)
         
     }
     
-    func startNewRound() {
-        countdown = 30
-        updateCounter()
-        peopleLetIn = 0
-        lives = 3
-        selectRandomPerson()
-    }
-    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
-    var letInLabel: UILabel = {
+    var scoreLabel: UILabel = {
         let label = UILabel()
         label.textColor = .white
-        label.text = "0 people let in"
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        return label
-    }()
-    
-    var timerLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.text = "30"
-        label.font = UIFont.boldSystemFont(ofSize: 28)
+        label.text = "0"
+        label.font = UIFont.boldSystemFont(ofSize: 36)
         return label
     }()
     
@@ -120,7 +60,6 @@ class GameViewController: UIViewController {
         return view
     }()
     
-
     let IDCardContainer: IDCardContainerView = {
         let view = IDCardContainerView()
         return view
@@ -144,14 +83,25 @@ class GameViewController: UIViewController {
         return button
     }()
     
-    
-    
     let dynamicButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(#imageLiteral(resourceName: "next"), for: .normal)
-        button.imageView?.contentMode = .scaleToFill
+//        button.setImage(#imageLiteral(resourceName: "next"), for: .normal)
+//        button.imageView?.contentMode = .scaleToFill
         return button
     }()
+    
+    let backgroundImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "background")
+        return iv
+    }()
+    
+    let tableImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "tableonly")
+        return iv
+    }()
+
     
     func handleMatch(sender: UIButton!) {
         // deny tag = 0, approve tag = 1
@@ -166,28 +116,27 @@ class GameViewController: UIViewController {
         }
         
         if isMatch == true && sender.tag == 0 {
-            lives -= 1
             showIncorrectResponseAlert(getLost: true)
         }
         
         if isMatch == true && sender.tag == 1  {
+            score += 1
             selectRandomPerson()
-            peopleLetIn += 1
             slideInIDCardContainer()
         }
         
         if isMatch == false && sender.tag == 0 {
+            score += 1
             selectRandomPerson()
             slideInIDCardContainer()
         }
         
         if isMatch == false && sender.tag == 1 {
-            lives -= 1
             showIncorrectResponseAlert(getLost: false)
         }
     }
     
-    func showIncorrectResponseAlert(getLost: Bool) {
+    fileprivate func showIncorrectResponseAlert(getLost: Bool) {
         
         let getLostTitle = "Hey! What did you do that for?! You're costing us money!"
         let wrongLetInTitle = "Hey! You let someone in you weren't supposed to!"
@@ -195,7 +144,7 @@ class GameViewController: UIViewController {
         let nextRoundAction = UIAlertAction(title: "Sorry! It won't happen again!", style: .default) { (_) in
             self.selectRandomPerson()
             self.slideInIDCardContainer()
-
+            
         }
         answerAlert.addAction(nextRoundAction)
         
@@ -203,17 +152,6 @@ class GameViewController: UIViewController {
         
     }
     
-    let backgroundImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "background")
-        return iv
-    }()
-    
-    let tableImageView: UIImageView = {
-        let iv = UIImageView()
-        iv.image = #imageLiteral(resourceName: "tableonly")
-        return iv
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -223,8 +161,6 @@ class GameViewController: UIViewController {
         fetchPeople()
         
         setupLayout()
-        
-        var _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         
         selectRandomPerson()
         slideInIDCardContainer()
@@ -241,8 +177,7 @@ class GameViewController: UIViewController {
         view.addSubview(personImage)
         view.addSubview(approveButton)
         view.addSubview(denyButton)
-        view.addSubview(letInLabel)
-        view.addSubview(timerLabel)
+        view.addSubview(scoreLabel)
         
         backgroundImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
@@ -258,27 +193,15 @@ class GameViewController: UIViewController {
         
         approveButton.anchor(top: dynamicButton.topAnchor, left: dynamicButton.rightAnchor, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: -4, paddingBottom: 4, paddingRight: 0, width: 90, height: 0)
         
+        scoreLabel.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 42, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
-        letInLabel.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 36, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
-        
-        timerLabel.anchor(top: letInLabel.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 30)
-        timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-//
-//                IDCard.anchor(top: tableImageView.topAnchor , left: nil, bottom: nil , right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 240, height: 180)
-//                IDCard.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         setupIDCard()
-        
-        //        IDCard.center = CGPoint(view.frame.size.width / 2), tableImageView.frame.size.height / 3)
-
-        
-        
-        
-        
+  
     }
     
-    func setupIDCard() {
+    fileprivate func setupIDCard() {
         
         IDCardContainer.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height / 1.39)
         
@@ -287,11 +210,17 @@ class GameViewController: UIViewController {
         IDCardContainer.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
     
-    func slideInIDCardContainer() {
+    fileprivate func slideInIDCardContainer() {
         IDCardContainer.center.x -= view.bounds.width
         
-        UIView.animate(withDuration: 0.5) {
+//        UIView.animate(withDuration: 0.5) {
+//            self.IDCardContainer.center.x += self.view.bounds.width
+//        }
+        
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
             self.IDCardContainer.center.x += self.view.bounds.width
+        }) { (_) in
+            // completion closure kept if needed in future
         }
     }
     
@@ -311,7 +240,7 @@ class GameViewController: UIViewController {
         guard let IDCardContainerView = sender.view else { return }
         guard let IDCardContainerViewCenter = IDCardCenter else { return }
         let point = sender.translation(in: view)
-        IDCardContainer.center = CGPoint(x: IDCardContainerViewCenter.x + point.x, y: IDCardContainerViewCenter.y + point.y)
+        IDCardContainer.center = CGPoint(x: IDCardContainerViewCenter.x + point.x, y: IDCardContainerViewCenter.y)
         
         if sender.state == UIGestureRecognizerState.ended {
             
@@ -323,10 +252,7 @@ class GameViewController: UIViewController {
         }
     }
     
-    
-    
-    
-    func fetchPeople() {
+    fileprivate func fetchPeople() {
         guard let path = Bundle.main.path(forResource: "People", ofType: "json") else { return }
         let url = URL(fileURLWithPath: path)
         
@@ -347,14 +273,7 @@ class GameViewController: UIViewController {
         }
     }
     
-    func updateCounter() {
-        if countdown > 0 {
-            countdown -= 1
-            timerLabel.text = "\(countdown)"
-        }
-    }
-    
-    // first make a copy of people array so we can remove the random person selected for the large square. The person is removed because we handle the option that the people match using a random number between 1 and 100. If the number less than 80, the smaller square is set to the same color as the large square (a match) and if the number is greater than 60, a random color is chosen from the 15 remaining colors in the array.
+     // first make a copy of people array so we can remove the random person selected for the large square. The person is removed because we handle the option that the people match using a random number between 1 and 100. If the number less than 80, the smaller square is set to the same color as the large square (a match) and if the number is greater than 60, a random color is chosen from the 15 remaining colors in the array.
     
     var randomPerson: Person?
     
---
+++ ColorGame/Sound.swift
@@ -0,0 +1,42 @@
+//
+//  Sound.swift
+//  ColorGame
+//
+//  Created by Zach Crystal on 2017-06-13.
+//  Copyright © 2017 Zach Crystal. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class Sound: AVPlayer {
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    func playMusic() {
+        guard let sound = NSDataAsset(name: "MusicLoop3") else {
+            print("asset not found")
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(data: sound.data, fileTypeHint: AVFileTypeMPEGLayer3)
+            
+            audioPlayer!.play()
+            audioPlayer!.numberOfLoops = -1
+            
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+    }
+    
+    func stopAudio() {
+        if audioPlayer != nil {
+            audioPlayer?.stop()
+            audioPlayer = nil
+        }
+    }
+}
